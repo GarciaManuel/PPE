c264f76ef72f678e898dc691f6e8c166cca5854d
Sofia <mariana_sofia.333@hotmail.com>
2020-02-19 14:32:28 +0100

    new branches

--- src/resolvers/mutation.ts
+++ src/resolvers/mutation.ts
@@ -1,45 +1,103 @@
 import { IResolvers } from "graphql-tools";
 import { Datetime } from "../lib/datetime";
-import bcryptjs from 'bcryptjs';
+import bcryptjs from "bcryptjs";
 const mutation: IResolvers = {
-    Mutation: {
-        async register(_: void, { user }, { db }): Promise<any> {
+  Mutation: {
+    async register(_: void, { user }, { db }): Promise<any> {
+      const userCheck = await db
+        .collection("users")
+        .findOne({ email: user.email });
 
-            const userCheck = await db.collection('users').findOne({ email: user.email });
-
-            if (userCheck !== null) {
-                return {
-                    status: false,
-                    message: `Usuario NO registrado porque ya existe el usuario ${user.email}`,
-                    user: null
-                };
-            }
-            const lastUser = await db.collection('users').find()
-                .limit(1).sort({ registerDate: -1 }).toArray();
-            if (lastUser.length === 0) {
-                user.id = 1;
-            } else {
-                user.id = lastUser[0].id + 1;
-            }
-            user.password = bcryptjs.hashSync(user.password, 10);
-            user.registerDate = new Datetime().getCurrentDateTime();
-            return await db.collection('users').insertOne(user)
-                .then((result: any) => {
-                    return {
-                        status: true,
-                        message: `Usuario ${user.name} ${user.lastname} añadido correctamente`,
-                        user
-                    };
-                })
-                .catch((err: any) => {
-                    return {
-                        status: false,
-                        message: `Usuario NO añadido correctamente`,
-                        user: null
-                    };
-                });
-        }
+      if (userCheck !== null) {
+        return {
+          status: false,
+          message: `Usuario NO registrado porque ya existe el usuario ${user.email}`,
+          user: null
+        };
+      }
+      const lastUser = await db
+        .collection("users")
+        .find()
+        .limit(1)
+        .sort({ registerDate: -1 })
+        .toArray();
+      if (lastUser.length === 0) {
+        user.id = 1;
+      } else {
+        user.id = lastUser[0].id + 1;
+      }
+      user.password = bcryptjs.hashSync(user.password, 10);
+      user.registerDate = new Datetime().getCurrentDateTime();
+      return await db
+        .collection("users")
+        .insertOne(user)
+        .then((result: any) => {
+          return {
+            status: true,
+            message: `Usuario ${user.name} ${user.lastname} añadido correctamente`,
+            user
+          };
+        })
+        .catch((err: any) => {
+          return {
+            status: false,
+            message: `Usuario NO añadido correctamente`,
+            user: null
+          };
+        });
+    },
+    async addMeasure(_: void, { measure }, { db }): Promise<any> {
+      console.log(measure);
+      measure.date = new Datetime().getCurrentDateTime();
+      return await db
+        .collection("measures")
+        .insertOne(measure)
+        .then((result: any) => {
+          return {
+            status: true,
+            message: `Successful addition to measures.`,
+            measure
+          };
+        })
+        .catch((err: any) => {
+          return {
+            status: false,
+            message: `Failed addition to measures.`,
+            measure: null
+          };
+        });
+    },
+    async deleteMeasure(_: void, { measureId }, { db, token }): Promise<any> {
+      let info: any = new JWT().verify(token);
+      if (info === "Invalid token. You need to log in first.") {
+        return {
+          status: false,
+          message: info,
+          user: null
+        };
+      }
+      return await db
+        .collection("measures")
+        .deleteOne({ id: measureId })
+        .then((result: any) => {
+          console.log(result.result);
+          let done = result.result.n && result.result.ok;
+          let deleted = "";
+          if (!done) deleted = "NO";
+          return {
+            status: done,
+            message: `Measure ${deleted} successfully deleted.`
+          };
+        })
+        .catch((err: any) => {
+          console.log(err);
+          return {
+            status: false,
+            message: `Failed attempt to delete measure.`
+          };
+        });
     }
-}
+  }
+};
 
 export default mutation;
--- src/resolvers/query.ts
+++ src/resolvers/query.ts
@@ -1,52 +1,65 @@
 import { IResolvers } from "graphql-tools";
 import JWT from "../lib/jwt";
-import bcryptjs from 'bcryptjs';
+import bcryptjs from "bcryptjs";
 
-const query : IResolvers = {
-    Query : {
-        async users (_: void, __: any, { db }): Promise<any> {
-            return await db.collection('users').find().toArray();
-        },
-        async login(_: void, { email, password }, { db }): Promise<any> {
-            const user = await db.collection('users').findOne({email});
-            if (user === null) {
-                return {
-                    status: false,
-                    message: 'Login INCORRECTO. No existe el usuario',
-                    token: null
-                }
-            }
+const query: IResolvers = {
+  Query: {
+    async users(_: void, __: any, { db }): Promise<any> {
+      return await db
+        .collection("users")
+        .find()
+        .toArray();
+    },
+    async login(_: void, { email, password }, { db }): Promise<any> {
+      const user = await db.collection("users").findOne({ email });
+      if (user === null) {
+        return {
+          status: false,
+          message: "Login INCORRECTO. No existe el usuario",
+          token: null
+        };
+      }
 
-            if (!bcryptjs.compareSync(password, user.password)) {
-                return {
-                    status: false,
-                    message: 'Login INCORRECTO. Contraseña incorrecta',
-                    token: null
-                }
-            }
-            delete user.password;
-            return {
-                status: true,
-                message: 'Login Correcto',
-                token: new JWT().sign({ user })
-            }
-        },
-        me(_: void, __: any, { token }) {
-            let info: any = new JWT().verify(token);
-            if (info === 'La autenticación del token es inválida. Por favor, inicia sesión para obtener un nuevo token') {
-                return {
-                    status: false,
-                    message: info,
-                    user: null
-                }
-            }
-            return {
-                status: true,
-                message: 'Token correcto',
-                user: info.user
-            }
-        }
+      if (!bcryptjs.compareSync(password, user.password)) {
+        return {
+          status: false,
+          message: "Login INCORRECTO. Contraseña incorrecta",
+          token: null
+        };
+      }
+      delete user.password;
+      return {
+        status: true,
+        message: "Login Correcto",
+        token: new JWT().sign({ user })
+      };
+    },
+    me(_: void, __: any, { token }) {
+      let info: any = new JWT().verify(token);
+      if (
+        info ===
+        "La autenticación del token es inválida. Por favor, inicia sesión para obtener un nuevo token"
+      ) {
+        return {
+          status: false,
+          message: info,
+          user: null
+        };
+      }
+      return {
+        status: true,
+        message: "Token correcto",
+        user: info.user
+      };
+    },
+    async getMeasures(patient: String, { db }): Promise<any> {
+      console.log("here");
+      return await db
+        .collection("measures")
+        .find({ patientId: patient })
+        .toArray();
     }
-}
+  }
+};
 
 export default query;
