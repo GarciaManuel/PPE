76f8116bb043b67a17be1ff70721a5c6457b70e9
Garcia M <garciam@MacBook-Pro.local>
2020-04-16 13:53:59 -0500

    Comments for the code

--- src/config/database.ts
+++ src/config/database.ts
@@ -1,12 +1,16 @@
 let mongo = require('mongodb');
 let MongoClient = mongo.MongoClient;
 import chalk from 'chalk';
+
+//Database class for sigelton aproach
 class Database {
 
+    //Init the database by reaching the mongoDB and the path on the env file
     async init() {
         const MONGODB = String(process.env.DATABASE);
         const client = await MongoClient.connect(MONGODB, { useNewUrlParser: true, useUnifiedTopology: true});
 
+        //Wait for the connection
         const db = await client.db();
 
         if ( client.isConnected() ) {
